22577a827780a754a45ec603f5c19e6e758515c2
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-10-22 15:55:40 -0300

    feature: orixa list

---
+++ Domain/Religion/Structs/Orixas.cs
@@ -0,0 +1,19 @@
+namespace Lira.Domain.Religion.Structs;
+
+public readonly struct Orixa
+{
+    public const string Oxala = "Oxalá";
+    public const string Oya = "Oyá Tempo";
+    public const string Oxum = "Oxum";
+    public const string Oxumare = "Oxumaré";
+    public const string Oxossi = "Oxóssi";
+    public const string Oba = "Obá";
+    public const string Xango = "Xangô";
+    public const string Iansa = "Iansã";
+    public const string Ogum = "Ogum";
+    public const string Egunita = "Egunitá";
+    public const string Obaluaie = "Obaluaiê";
+    public const string Nana = "Nanã";
+    public const string Iemanja = "Iemanjá";
+    public const string Omolu = "Omolu";
+}
