e830182a28252b8360085482f8eb9214026ac2e8
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-11-02 20:12:05 -0300

    refactor

--- Application/CQRS/People/Commands/RegisterPerson/RegisterPersonResponse.cs
+++ Application/CQRS/People/Commands/RegisterPerson/RegisterPersonResponse.cs
@@ -3,11 +3,9 @@ namespace Lira.Application.CQRS.Accounts.Commands.RegisterMedium;
 public class RegisterPersonResponse
 {
     public Guid Id { get; init; }
-    public Guid PersonId { get; init; }
 
-    public RegisterPersonResponse(Guid id, Guid personId)
+    public RegisterPersonResponse(Guid id)
     {
         Id = id;
-        PersonId = personId;
     }
 }
