b68f99037e63f3dae54886c074dc6079b6d844b5
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-11-04 20:59:58 -0300

    manager message

--- Application/Messages/ManagerMessages.cs
+++ Application/Messages/ManagerMessages.cs
@@ -7,5 +7,4 @@ public readonly struct ManagerMessages
     public const string InvalidUsernameOrPassword = "User or password is invalid.";
     public const string PasswordsDoNotMatch = "Passwords do not match";
     public const string PasswordIsInvalid = "Password is invalid";
-    public const string UsernameIsInUse = "Username is in use";
 }
