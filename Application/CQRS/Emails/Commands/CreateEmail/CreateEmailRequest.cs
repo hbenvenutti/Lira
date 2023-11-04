ceca301983e7b92eb0afab479ea2b84513a9df31
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-11-04 20:58:48 -0300

    conflict messages

--- Application/CQRS/Emails/Commands/CreateEmail/CreateEmailRequest.cs
+++ Application/CQRS/Emails/Commands/CreateEmail/CreateEmailRequest.cs
@@ -1,23 +1,24 @@
-using Lira.Application.Responses;
 using Lira.Domain.Enums;
-using MediatR;
 
-namespace Lira.Application.CQRS.Emails.Commands.CreateEmail;
+namespace Lira.Application.CQRS.Email.Commands.CreateEmail;
 
-public class CreateEmailRequest : IRequest<Response<CreateEmailResponse>>
+public class CreateEmailRequest
 {
-    public Guid PersonId { get; init; }
     public string Address { get; init; }
     public EmailType Type { get; init; }
+    public Guid PersonId { get; init; }
+    public bool ValidatePersonId { get; init; }
 
     public CreateEmailRequest(
-        Guid personId,
         string address,
-        EmailType type
+        EmailType type,
+        Guid personId,
+        bool validatePersonId = true
     )
     {
-        PersonId = personId;
         Address = address;
         Type = type;
+        PersonId = personId;
+        ValidatePersonId = validatePersonId;
     }
 }
--- Application/Messages/PersonMessages.cs
+++
@@ -1,9 +0,0 @@
-namespace Lira.Application.Messages;
-
-public readonly struct PersonMessages
-{
-    public const string PersonAlreadyExists
-        = "Person with this document already exists.";
-
-    public const string PersonIdIsInvalid = "Person id is invalid.";
-}
