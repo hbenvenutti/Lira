0f54b29cffaf1144b80099aec268ef8b04d61df8
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-11-04 18:32:38 -0300

    create person orixa handler

---
+++ Application/CQRS/PersonOrixa/Commands/CreatePersonOrixa/CreatePersonOrixaRequest.cs
@@ -0,0 +1,27 @@
+using Lira.Application.Responses;
+using Lira.Domain.Religion.Enums;
+using MediatR;
+
+namespace Lira.Application.CQRS.PersonOrixa.Commands.CreatePersonOrixa;
+
+public class CreatePersonOrixaRequest :
+    IRequest<Response<CreatePersonOrixaResponse>>
+{
+    public Guid PersonId { get; init; }
+    public Guid OrixaId { get; init; }
+    public OrixaType Type { get; init; }
+    public bool ValidatePerson { get; init; }
+
+    public CreatePersonOrixaRequest(
+        Guid personId,
+        Guid orixaId,
+        OrixaType type,
+        bool validatePerson = true
+    )
+    {
+        PersonId = personId;
+        OrixaId = orixaId;
+        Type = type;
+        ValidatePerson = validatePerson;
+    }
+}
