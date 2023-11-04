f04157d53dfe47706158f3d65f27193cfad3687e
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-11-04 20:01:40 -0300

    email explicit operators

--- Data/Entities/EmailEntity.cs
+++ Data/Entities/EmailEntity.cs
@@ -1,3 +1,6 @@
+using Lira.Data.Enums;
+using Lira.Domain.Domains.Email;
+using Lira.Domain.Domains.Person;
 using Lira.Domain.Enums;
 
 namespace Lira.Data.Entities;
@@ -15,6 +18,45 @@ public class EmailEntity : BaseEntity
 
     public required Guid PersonId { get; set; }
     public PersonEntity? Person { get; set; }
+
+    # endregion
+
+    # region ---- operator -----------------------------------------------------
+
+    public static explicit operator EmailEntity(EmailDomain domain) => new()
+    {
+        Id = domain.Id,
+        Address = domain.Address,
+        Type = domain.Type,
+        PersonId = domain.PersonId,
+        Person = null,
+        CreatedAt = domain.CreatedAt,
+        UpdatedAt = domain.UpdatedAt,
+        DeletedAt = domain.DeletedAt,
+        Status = (EntityStatus) domain.Status
+    };
+
+    public static explicit operator EmailDomain(EmailEntity entity)
+    {
+        if (entity.Person is not null)
+        {
+            entity.Person.Emails = null;
+        }
+
+        return new EmailDomain(
+            id: entity.Id,
+            address: entity.Address,
+            type: entity.Type,
+            createdAt: entity.CreatedAt,
+            personId: entity.PersonId,
+            person: entity.Person is null
+                ? null
+                : (PersonDomain) entity.Person,
+            status: (DomainStatus) entity.Status,
+            updatedAt: entity.UpdatedAt,
+            deletedAt: entity.DeletedAt
+        );
+    }
 
     # endregion
 }
