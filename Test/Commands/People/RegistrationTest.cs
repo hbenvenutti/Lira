f1af5d7d90eec6d2a638a50b06c2ebc6c76c1423
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-11-04 21:37:18 -0300

    registration test

---
+++ Test/Commands/People/RegistrationTest.cs
@@ -0,0 +1,213 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using Lira.Application.CQRS.Address.Commands.CreateAddress;
+using Lira.Application.CQRS.Emails.Commands.CreateEmail;
+using Lira.Application.CQRS.Medium.Commands.CreateMedium;
+using Lira.Application.CQRS.People.Commands.CreatePerson;
+using Lira.Application.CQRS.People.Commands.RegisterPerson;
+using Lira.Application.CQRS.PersonOrixa.Commands.CreatePersonOrixa;
+using Lira.Application.CQRS.Phone.Commands.CreatePhone;
+using Lira.Application.Enums;
+using Lira.Application.Responses;
+using Lira.Domain.Enums;
+using MediatR;
+using Moq;
+
+namespace Lira.Test.Commands.People;
+
+[ExcludeFromCodeCoverage]
+public class RegistrationTest
+{
+    private readonly Mock<IMediator> _mediatorMock;
+
+    private readonly RegisterPersonHandler _registerPersonHandler;
+    private readonly RegisterPersonRequest _registerPersonRequest;
+
+    # region ---- constants ----------------------------------------------------
+
+    private static readonly Guid PersonId = Guid.NewGuid();
+
+    private const string FirstName = "John";
+    private const string Surname = "Doe";
+    private const string Document = "12345678900";
+    private const string Email = "johndoe@gmail.com";
+    private const string PhoneNumber = "11999999999";
+    private const string Street = "Rua dos Bobos";
+    private const string Number = "0";
+    private const string Neighborhood = "Centro";
+    private const string City = "São Paulo";
+    private const string State = "SP";
+    private const string ZipCode = "94477453";
+    private const string Complement = "Apto 123";
+    private const bool IsMedium = true;
+    private static readonly DateTime? FirstAmaci = DateTime.UtcNow;
+    private static readonly DateTime? LastAmaci = DateTime.UtcNow;
+    private static readonly Guid? FrontOrixaId = Guid.NewGuid();
+    private static readonly Guid? AdjunctOrixaId = Guid.NewGuid();
+    private static readonly Guid? AncestralOrixaId = Guid.NewGuid();
+    private static readonly EmailType? EmailType = 0;
+
+    # endregion
+
+    # region ---- constructor --------------------------------------------------
+
+    public RegistrationTest()
+    {
+        _mediatorMock = new Mock<IMediator>();
+
+        _registerPersonHandler = new RegisterPersonHandler(
+            _mediatorMock.Object
+        );
+
+        SetupMediatorMock();
+
+        _registerPersonRequest = new RegisterPersonRequest(
+            firstName: FirstName,
+            surname: Surname,
+            document: Document,
+            email: Email,
+            phoneNumber: PhoneNumber,
+            street: Street,
+            number: Number,
+            neighborhood: Neighborhood,
+            city: City,
+            state: State,
+            zipCode: ZipCode,
+            complement: Complement,
+            isMedium: IsMedium,
+            firstAmaci: FirstAmaci,
+            lastAmaci: LastAmaci,
+            frontOrixaId: FrontOrixaId,
+            adjunctOrixaId: AdjunctOrixaId,
+            ancestralOrixaId: AncestralOrixaId,
+            emailType: EmailType
+        );
+    }
+
+    # endregion
+
+    # region ---- mock setup ---------------------------------------------------
+
+    private void SetupMediatorMock()
+    {
+        _mediatorMock
+            .Setup(mediator => mediator.Send(
+                It.IsAny<CreatePersonRequest>(),
+                It.IsAny<CancellationToken>()
+            ))
+            .ReturnsAsync(new Response<CreatePersonResponse>(
+                isSuccess: true,
+                httpStatusCode: HttpStatusCode.Created,
+                statusCode: StatusCode.CreatedOne,
+                data: new CreatePersonResponse(
+                    id: PersonId
+                )
+            ));
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(
+                It.IsAny<CreateAddressRequest>(),
+                It.IsAny<CancellationToken>()
+            ))
+            .ReturnsAsync(new Response<CreateAddressResponse>(
+                isSuccess: true,
+                httpStatusCode: HttpStatusCode.Created,
+                statusCode: StatusCode.CreatedOne,
+                data: new CreateAddressResponse(
+                    id: Guid.NewGuid()
+                )
+            ));
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(
+                It.IsAny<CreateEmailRequest>(),
+                It.IsAny<CancellationToken>()
+            ))
+            .ReturnsAsync(new Response<CreateEmailResponse>(
+                isSuccess: true,
+                httpStatusCode: HttpStatusCode.Created,
+                statusCode: StatusCode.CreatedOne,
+                data: new CreateEmailResponse(
+                    id: Guid.NewGuid()
+                )
+            ));
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(
+                It.IsAny<CreatePhoneRequest>(),
+                It.IsAny<CancellationToken>()
+            ))
+            .ReturnsAsync(new Response<CreatePhoneResponse>(
+                isSuccess: true,
+                httpStatusCode: HttpStatusCode.Created,
+                statusCode: StatusCode.CreatedOne,
+                data: new CreatePhoneResponse(
+                    id: Guid.NewGuid()
+                )
+            ));
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(
+                It.IsAny<CreatePersonOrixaRequest>(),
+                It.IsAny<CancellationToken>()
+            ))
+            .ReturnsAsync(new Response<CreatePersonOrixaResponse>(
+                isSuccess: true,
+                httpStatusCode: HttpStatusCode.Created,
+                statusCode: StatusCode.CreatedOne,
+                data: new CreatePersonOrixaResponse(
+                    id: Guid.NewGuid()
+                )
+            ));
+
+        _mediatorMock
+            .Setup(mediator => mediator.Send(
+                It.IsAny<CreateMediumRequest>(),
+                It.IsAny<CancellationToken>()
+            ))
+            .ReturnsAsync(new Response<CreateMediumResponse>(
+                isSuccess: true,
+                httpStatusCode: HttpStatusCode.Created,
+                statusCode: StatusCode.CreatedOne,
+                data: new CreateMediumResponse(
+                    id: Guid.NewGuid()
+                )
+            ));
+    }
+
+    # endregion
+
+    # region ---- register person ----------------------------------------------
+
+    [Fact]
+    public async void ShouldRegisterAPerson()
+    {
+        var result = await _registerPersonHandler.Handle(
+            _registerPersonRequest,
+            CancellationToken.None
+        );
+
+        Assert.True(result.IsSuccess);
+        Assert.Equal(
+            expected: HttpStatusCode.Created,
+            actual:result.HttpStatusCode
+        );
+
+        Assert.Equal(
+            expected: StatusCode.CreatedTransaction,
+            actual: result.StatusCode
+        );
+
+        Assert.NotNull(result.Data);
+
+        Assert.Equal(
+            expected: PersonId,
+            actual: result.Data.Id
+        );
+
+        Assert.Null(result.Error);
+        Assert.Null(result.Pagination);
+    }
+
+    # endregion
+}
