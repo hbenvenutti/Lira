9aefd89881399bdc4f2d9b8cfc4596edaa641fa6
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-11-19 19:12:32 -0300

    refactor: changed indent

--- Web/Program.cs
+++ Web/Program.cs
@@ -11,8 +11,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler(
+        errorHandlingPath: "/Error",
+        createScopeForErrors: true
+    );
+
+    // The default HSTS value is 30 days. You may want to change this for
+    // production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
