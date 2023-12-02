ce7de973e1f0e6fe52ad0618752dd94a6068bf7e
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-12-02 17:07:55 -0300

    feat: app status

--- Application/Enums/AppStatusCode.cs
+++ Application/Enums/AppStatusCode.cs
@@ -4,66 +4,126 @@ public enum AppStatusCode
 {
     Empty = 0,
 
-    # region ---- login --------------------------------------------------------
+    # region ---- login 10_xx-xxx ----------------------------------------------
 
-    SignInSuccess = 120001,
-    SignInFailed  = 140001,
-
-    # endregion
-
-    # region ---- created ------------------------------------------------------
-
-    CreatedOne = 220001,
-    CreatedMany = 220002,
-    CreatedTransaction = 220003,
-    CreatedWithErrors = 240001,
+    SignInSuccess = 10_00_200,
+    SignInFailed  = 10_00_404,
+    PasswordsDoNotMatch = 10_10_400,
+    InvalidPassword = 10_11_400,
 
     # endregion
 
-    # region ---- missing resources --------------------------------------------
+    # region ---- generic 11-xx-xxx --------------------------------------------
 
-    ManagerNotFound = 340001,
-    PersonNotFound = 340002,
-    OrixaNotFound = 340008,
-    PersonOrixaNotFound = 340009,
+    CreatedOne = 11_00_201,
+    CreatedMany = 11_01_201,
+    CreatedTransaction = 11_03_201,
+    CreatedWithErrors = 11_04_201,
 
-    # endregion
-
-    # region ---- invalid data -------------------------------------------------
-
-    PasswordsDoNotMatch = 440001,
-    InvalidPassword = 440002,
-    InvalidName = 440003,
-    InvalidSurname = 440004,
-    InvalidCpf = 440005,
-    InvalidUsername = 440006,
-    InvalidUf = 440007,
-    InvalidZipCode = 440008,
-    InvalidStreet = 440010,
-    InvalidPhone = 440011,
-    InvalidAddressNumber = 440012,
-    InvalidNeighborhood = 440013,
-    InvalidCity = 440014,
-    InvalidEmailAddress = 440015,
-
-    SeveralInvalidFields = 440090,
-    InvalidAdminCode = 440099,
-    OneOrMoreSpecificationsFailed = 440100,
+    SeveralInvalidFields = 11_01_400,
 
     # endregion
 
-    # region ---- conflict -----------------------------------------------------
+    # region ---- person 20-xx-xxx ---------------------------------------------
 
-    AdminAlreadyExists = 540001,
-    PersonAlreadyExists = 540002,
-    UsernameAlreadyExists = 540003,
-    EmailAlreadyExists = 540004,
+    CreatedPerson = 20_00_201,
+    UpdatedPerson = 20_01_200,
+    DeletedPerson = 20_02_204,
+
+    InvalidCpf = 20_10_400,
+    InvalidName = 20_11_400,
+    InvalidSurname = 20_12_400,
+    CpfInUse = 20_10_409,
+
+    PersonNotFound = 20_20_404,
+    PersonAlreadyExists = 20_20_409,
 
     # endregion
 
-    # region ---- error --------------------------------------------------------
+    # region ---- manager 21-xx-xxx --------------------------------------------
 
-    UnexpectedError = 950001
+    CreatedManager = 21_00_201,
+    UpdatedManager = 21_01_200,
+    DeletedManager = 21_02_204,
+
+    InvalidUsername = 21_10_400,
+    UsernameAlreadyExists = 21_10_409,
+
+    ManagerNotFound = 21_20_404,
+
+    AdminAlreadyExists = 21_90_409,
+    InvalidAdminCode = 21_90_400,
+
+    # endregion
+
+    # region ---- email 22_xx-xxx ----------------------------------------------
+
+    CreatedEmail = 22_00_201,
+    UpdatedEmail = 22_01_200,
+    DeletedEmail = 22_02_204,
+    InvalidEmailAddress = 22_10_400,
+    EmailAlreadyExists = 22_10_409,
+    EmailNotFound = 22_20_404,
+
+    # endregion
+
+    # region ---- address 23_xx-xxx --------------------------------------------
+
+    CreatedAddress = 23_00_201,
+    UpdatedAddress = 23_01_200,
+    DeletedAddress = 23_02_204,
+
+    InvalidUf = 23_10_400,
+    InvalidStreet = 23_11_400,
+    InvalidZipCode = 23_12_400,
+    InvalidNeighborhood = 23_13_400,
+    InvalidAddressNumber = 23_14_400,
+    InvalidCity = 23_15_400,
+
+    AddressNotFound = 23_20_404,
+
+    # endregion
+
+    # region ---- phone 24-xx-xxx ----------------------------------------------
+
+    CreatedPhone = 24_00_201,
+    UpdatedPhone = 24_01_200,
+    DeletedPhone = 24_02_204,
+
+    InvalidPhone = 24_10_400,
+
+    PhoneNotFound = 24_20_404,
+
+    # endregion
+
+    # region ---- orixa 25-xx-xxx ----------------------------------------------
+
+    CreatedOrixa = 25_00_201,
+    UpdatedOrixa = 25_01_200,
+    DeletedOrixa = 25_02_204,
+
+    InvalidOrixa = 25_10_400,
+
+    OrixaNotFound = 25_20_404,
+
+    # endregion
+
+    # region ---- person orixa 26-xx-xxx ---------------------------------------
+
+    CreatedPersonOrixa = 26_00_201,
+    UpdatedPersonOrixa = 26_01_200,
+    DeletedPersonOrixa = 26_02_204,
+
+    InvalidPersonOrixa = 26_10_400,
+
+    PersonOrixaNotFound = 26_20_404,
+
+    # endregion
+
+    # region ---- error 9x_xx-xxx ----------------------------------------------
+
+    UnexpectedError = 90_00_500,
+    BadGateway = 90_00_502
 
     # endregion
 }
